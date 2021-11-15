995f28097b325be2e3f12eba09c4f5281a68ed9b
Alexander Sihvo <mealex.sihvo@gmail.com>
2021-11-15 14:48:53 +0200

    Added lines in restaurant model and route

--- models/restaurants.js
+++ models/restaurants.js
@@ -2,6 +2,15 @@ const db = require('../lib/database');
 
 const restaurants = {
 
+  getAllRestaurants: function(callback) {
+    return db.query("select idrestaurants, image, name, address, restaurant_type, operating_hours, price_level from restaurants", callback);
+  },
+
+  getManagerRestaurants: function(userId, callback) {
+    return db.query("select * from restaurants join users ON \
+    restaurants.users_idusers = users.idusers where idusers=? AND account_type = 'manager'", [userId], callback);
+  },
+
   createRestaurant: function(settings, callback) {
     return db.query("insert into restaurants (idrestaurants, name, address, image, \
     restaurant_type, operating_hours, price_level, restaurant_Description, users_idUsers) VALUES (?,?,?,?,?,?,?,?,?)",
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,9 +2,35 @@ const express = require('express');
 const router = express.Router();
 const restaurants = require('../models/restaurants');
 
-router.get('/', function(req, res, next) {
-  res.send('Restaurants');
+
+router.get('/', 
+function(req, res) {
+    restaurants.getAllRestaurants(
+    function(err, dbResult) {
+      if(err) {
+        res.json(err);
+      }
+      else {
+        res.json(dbResult);
+      }
+    });
 });
+
+
+router.get('/manager',
+function(req, res) {
+    restaurants.getManagerRestaurants(req.body,
+    function(err, dbResult) {
+      if(err) {
+        res.json(err);
+      }
+      else {
+        res.json(dbResult);
+      }
+    });
+});
+
+
 
 router.post('/newRestaurant',
   function(req, res) {
