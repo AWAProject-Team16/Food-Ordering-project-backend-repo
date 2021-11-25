fb834b2f207e5870bd701f5562704b737b979c4e
Alexander Sihvo <mealex.sihvo@gmail.com>
2021-11-25 15:56:35 +0200

    Added few lines in order routes and models (not yet done)

--- models/orders.js
+++ models/orders.js
@@ -2,8 +2,28 @@ const db = require('../lib/database.js');
 
 const orders = {
 
+    // ORDERS -> FOREIGN KEYS -> FOREIGN KEY OPTIONS -> NO ACTIONS ON UPDATE AND ON DELETE FOR restaurant table
+
+    getUserOrders: function(userId, callback) {
+        return db.query("select idorders, order_status, order_total_cost, order_date from orders where users_idusers = ?", [userId], callback);
+    },
+
+
+    getUserOrderDetails: function(userId, orderId, callback) {
+        return db.query("select orders_idorders, product_name, product_cost from order_details JOIN orders ON order_details.orders_idorders = orders.idorders \
+        where orders.users_idusers = ? AND orders_idorders = ?", [userId, orderId], callback);
+    },
+
+
+    getRestaurantOrders: function(userId, restaurantId, callback) {
+        return db.query("select idorders, order_status, order_total_cost, order_date from orders \
+        JOIN restaurants ON orders.restaurants_idrestaurants = restaurants.idrestaurants where orders.users_idusers = ? \
+        OR restaurants.users_idusers = ? AND restaurants_idrestaurants = ?", [userId, userId, restaurantId], callback);
+    },
+
+
     getOrderById: function(id, userId, callback) {
-        return db.query("select * from orders where idorders=? AND users_idusers=?", [id, userId.userId], callback);
+        return db.query("select idorders, order_status, order_total_cost, order_date from orders where idorders=? AND users_idusers=?", [id, userId.userId], callback);
     },
 
     getOrdersByCustomer: function(userId, callback) {
--- routes/orders.js
+++ routes/orders.js
@@ -1,125 +1,189 @@
 const express = require('express');
 const router = express.Router();
 const orders = require('../models/orders');
-const db = require('../lib/database.js');
+const db = require('../lib/database');
 
-// Get order information by orderId
-router.get('/:id', 
+const passport = require('passport');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(cors())
+router.use(passport.initialize());
+
+// For normal user with account_type = 1 (not manager)
+router.post('/ordersHistory', passport.authenticate('basic', { session: false }),
 function(req, res) {
-    orders.getOrderById(req.params.id, req.body,
+  orders.getUserOrders(req.user,
     function(err, dbResult) {
       if(err) {
         res.status(500).json(err);
       }
       else {
         if(dbResult == '') {
-          res.status(200).json('Order not found');
+          res.status(400).json({"Status": 400 + ', Orders not found'});
         }
         else {
-          res.status(200).json({Order: dbResult})
+          res.status(200).json({Orders: dbResult});
         }
-
       }
     });
 });
 
-router.get('/:id',
+router.post('/ordersHistory/orderId/:idorders', passport.authenticate('basic', { session: false }),
 function(req, res) {
-  orders.getOrderStatus(req.params.id,
+  orders.getOrderDetails(req.user, req.params.idorders,
+    function(err, dbResult) {
+      if(err) {
+        res.status(500).json(err);
+      }
+      else {
+        if(dbResult == 'undefined') {
+          res.status(400).json({"Status": 400 + ", Order Id does not exist"});
+        }
+        else {
+          res.status(200).json(dbResult);
+        }
+      }
+    });
+});
+
+router.post('/ordersHistory/restaurant/:idrestaurants', passport.authenticate('basic', {session: false}),
+function(req, res) {
+  orders.getRestaurantOrders(req.user, req.params.idrestaurants,
     function(err, dbResult) {
       if(err) {
         res.status(500).json(err);
       }
       else {
         if(dbResult == '') {
-          res.status(200).json('Order not found');
+          res.status(400).json({"Status": 400 + ", Orders not found"});
         }
         else {
-          res.status(200).json({Status: dbResult});
+          res.status(200).json({Orders: dbResult});
         }
       }
     });
 });
 
-// Get all customer orders by userId
-router.get('/',
-function(req, res) {
-  orders.getOrdersByCustomer(req.body,
-  function(err, dbResult) {
-    if(err) {
-      res.status(500).json(err);
-    }
-    else {
-      if(dbResult == '') {
-        res.status(200).json('Orders not found');
-      }
-      else {
-        res.status(200).json({Orders: dbResult});
-      }
-    }
-  });
-});
 
-// Get all manager orders by userId
-router.get('/',
-function(req, res) {
-  orders.getOrdersByManager(req.body,
-  function(err, dbResult) {
-    if(err) {
-      res.status(500).json(err);
-    }
-    else {
-      if(dbResult == '') {
-        res.status(200).json('Orders not found')
-      }
-      else {      
-        res.status(200).json({Orders: dbResult});
-      }
+// // Get order information by orderId
+// router.get('/:id', 
+// function(req, res) {
+//     orders.getOrderById(req.params.id, req.body,
+//     function(err, dbResult) {
+//       if(err) {
+//         res.status(500).json(err);
+//       }
+//       else {
+//         if(dbResult == '') {
+//           res.status(200).json('Order not found');
+//         }
+//         else {
+//           res.status(200).json({Order: dbResult})
+//         }
 
-    }
-  });
-});
+//       }
+//     });
+// });
 
-// Get all restaurant orders by restaurantId
-router.get('/restaurant/:restaurantId',
-function(req, res) {
-  orders.getOrdersByRestaurant(req.params.restaurantId, req.body,
-  function(err, dbResult) {
-    if(err) {
-      res.status(500).json(err);
-    }
-    else {
-      if(dbResult == '') {
-        res.status(200).send('Orders not found');
-      }
-      else {      
-        res.status(200).json({Orders: dbResult});
-      }
+// router.get('/:id',
+// function(req, res) {
+//   orders.getOrderStatus(req.params.id,
+//     function(err, dbResult) {
+//       if(err) {
+//         res.status(500).json(err);
+//       }
+//       else {
+//         if(dbResult == '') {
+//           res.status(200).json('Order not found');
+//         }
+//         else {
+//           res.status(200).json({Status: dbResult});
+//         }
+//       }
+//     });
+// });
 
-    }
-  });
-});
+// // Get all customer orders by userId
+// router.get('/',
+// function(req, res) {
+//   orders.getOrdersByCustomer(req.body,
+//   function(err, dbResult) {
+//     if(err) {
+//       res.status(500).json(err);
+//     }
+//     else {
+//       if(dbResult == '') {
+//         res.status(200).json('Orders not found');
+//       }
+//       else {
+//         res.status(200).json({Orders: dbResult});
+//       }
+//     }
+//   });
+// });
 
-router.post('/',
-function(req, res) {
-  orders.addOrder(req.body,
-    function(err) {
-      if(err) {
-        res.status(500).json(err);
-      }
-      else {
-        res.status(201).json({Status: 'Order created'});
-        orders.addDetailedOrder(req.body,
-          function(err) {
-            if(err) {
-              res.status(400).json(err);
-            }
-            else {
-              res.status(201).json({Status: 'Extra details created'});
-            }
-          });
-      }
-    })
-})
+// // Get all manager orders by userId
+// router.get('/',
+// function(req, res) {
+//   orders.getOrdersByManager(req.body,
+//   function(err, dbResult) {
+//     if(err) {
+//       res.status(500).json(err);
+//     }
+//     else {
+//       if(dbResult == '') {
+//         res.status(200).json('Orders not found')
+//       }
+//       else {      
+//         res.status(200).json({Orders: dbResult});
+//       }
+
+//     }
+//   });
+// });
+
+// // Get all restaurant orders by restaurantId
+// router.get('/restaurant/:restaurantId',
+// function(req, res) {
+//   orders.getOrdersByRestaurant(req.params.restaurantId, req.body,
+//   function(err, dbResult) {
+//     if(err) {
+//       res.status(500).json(err);
+//     }
+//     else {
+//       if(dbResult == '') {
+//         res.status(200).send('Orders not found');
+//       }
+//       else {      
+//         res.status(200).json({Orders: dbResult});
+//       }
+
+//     }
+//   });
+// });
+
+// router.post('/',
+// function(req, res) {
+//   orders.addOrder(req.body,
+//     function(err) {
+//       if(err) {
+//         res.status(500).json(err);
+//       }
+//       else {
+//         res.status(201).json({Status: 'Order created'});
+//         orders.addDetailedOrder(req.body,
+//           function(err) {
+//             if(err) {
+//               res.status(400).json(err);
+//             }
+//             else {
+//               res.status(201).json({Status: 'Extra details created'});
+//             }
+//           });
+//       }
+//     })
+// })
 
 module.exports = router;
