b15c5877ef2cb8e2e1b37b683ac7f20727a0e7b2
Thuc Nguyen <t0ngdi00@students.oamk.fi>
2021-12-03 22:35:15 +0200

    #9 task 2/2 create category (backend) done

--- routes/products.js
+++ routes/products.js
@@ -12,19 +12,24 @@ router.use(passport.initialize());
 const upload = require("../lib/handleImages");
 
 // Gets all restaurant products. Works
-router.get("/restaurant/:idrestaurants", function (req, res) {
-  products.getAllProducts(req.params.idrestaurants, function (err, dbResult) {
-    if (err) {
-      res.status(500).json(err);
-    } else {
-      if (dbResult == "") {
-        res.status(400).json({ Status: 400 + ", No available products" });
-      } else {
-        res.status(200).json({ Products: dbResult });
+router.get('/restaurant/:idrestaurants',
+function(req, res) {
+  products.getAllProducts(req.params.idrestaurants,
+    function(err, dbResult) {
+      if(err) {
+        res.status(500).json(err);
+      }
+      else {
+        if(dbResult == '') {
+          res.status(400).json({"Status": 400 + ", No available products"});
+        }
+        else {
+          res.status(200).json({Products: dbResult});
+        }
       }
     }
-  });
-});
+  )
+})
 
 // Gets product by productId. Works
 router.get("/product/:product_id", function (req, res) {
@@ -46,37 +51,68 @@ router.get("/product/:product_id", function (req, res) {
 });
 
 // Gets all products by categoryId. Works
-router.get("/category/:category_id", function (req, res) {
-  products.getProductByCategory(
-    req.params.category_id,
-    function (err, dbResult) {
-      if (err) {
+router.get('/category/:category_id',
+function(req, res) {
+  products.getProductByCategory(req.params.category_id,
+    function(err, dbResult) {
+      if(err) {
         res.status(500).json(err);
-      } else {
-        if (dbResult == "") {
-          res
-            .status(400)
-            .json({
-              Status:
-                400 +
-                ", No products in category with this id: " +
-                req.params.category_id,
-            });
-        } else {
+      }
+      else {
+        if(dbResult == '') {
+          res.status(400).json({"Status": 400 + ", No products in category with this id: "+req.params.category_id});
+        }
+        else {
           res.status(200).json({ Products: dbResult });
         }
       }
-    }
-  );
+    });
 });
 
 // Adds a product to the selected category by categoryId. If the account does not have permission to do so, the product will not be added to the category. Works
+router.post('/category/:category_id/addProduct', passport.authenticate('jwt', { session: false }),
+function(req, res) {
+  products.checkPermissions(req.user.idusers, req.params.category_id,
+    function(err, dbResult) {
+      if(err) {
+        res.status(500).json(err);
+      }
+      else {
+        try {
+        if(dbResult[0]['account_type'] == 2) {
+          if(dbResult[0]['idcategories'] == req.params.category_id) {
+
+            products.addProduct(req.params.category_id, req.body, 
+              function(err, dbResult) {
+                if(err) {
+                  res.status(500).json(err);
+                }
+                else {
+                  if(dbResult.affectedRows == 0) {
+                    res.status(400).json({"Status": 400 + ", Something wrong with adding product in category with this id: "+req.params.category_id+". Try again or contact the IT-manager"});
+                  }
+                  else {
+                    res.status(201).json({"Status": 201 + ', Product added'});
+                  }
+              }
+            });
+        }
+      }
+    }catch{
+      res.status(400).json({"Status": 400 + ", No permissions for adding product in this category. For help, contact the IT-manager"});
+    }
+    }
+  });
+});
+
+// Adds a product to the selected category by categoryId (with image). Added by Thuc
 router.post(
-  "/category/:category_id/addProduct",
+  "/category/:category_id/addProductMultipart",
   passport.authenticate("jwt", { session: false }),
+  upload.single('product_image'),
   function (req, res) {
     products.checkPermissions(
-      req.user.user,
+      req.user.idusers,
       req.params.category_id,
       function (err, dbResult) {
         if (err) {
@@ -85,9 +121,12 @@ router.post(
           try {
             if (dbResult[0]["account_type"] == 2) {
               if (dbResult[0]["idcategories"] == req.params.category_id) {
-                products.addProduct(
+                const productInfo = {...req.body};
+                productInfo.product_image = req.file.filename;
+                
+                products.addProductMultipart(
                   req.params.category_id,
-                  req.body,
+                  productInfo,
                   function (err, dbResult) {
                     if (err) {
                       res.status(500).json(err);
@@ -127,186 +166,93 @@ router.post(
   }
 );
 
-// Adds a product to the selected category by categoryId (with image). Added by Thuc
-router.post(
-  "/category/:category_id/addProductMultipart",
-  passport.authenticate("jwt", { session: false }),
-  upload.single("product_image"),
-  function (req, res) {
-    products.checkPermissions(
-      req.user.idusers,
-      req.params.category_id,
-      function (err, dbResult) {
-        if (err) {
-          res.status(500).json(err);
-        } else {
-          try {
-            if (dbResult[0]["account_type"] == 2) {
-              if (dbResult[0]["idcategories"] == req.params.category_id) {
-                const productInfo = { ...req.body };
-                productInfo.product_image = req.file.filename;
-
-                products.addProductMultipart(
-                  req.params.category_id,
-                  productInfo,
-                  function (err, dbResult) {
-                    if (err) {
-                      res.status(500).json(err);
-                    } else {
-                      if (dbResult.affectedRows == 0) {
-                        res.status(400).json({
-                          Status:
-                            400 +
-                            ", Something wrong with adding product in category with this id: " +
-                            req.params.category_id +
-                            ". Try again or contact the IT-manager",
-                        });
-                      } else {
-                        res
-                          .status(201)
-                          .json({ Status: 201 + ", Product added" });
-                      }
-                    }
-                  }
-                );
-              }
-            }
-          } catch {
-            res.status(400).json({
-              Status:
-                400 +
-                ", No permissions for adding product in this category. For help, contact the IT-manager",
-            });
-          }
-        }
-      }
-    );
-  }
-);
-
 // Modifies product information by productId and categoryId. If the account does not have permission to do so, the product cannot be changed. Works
-router.post(
-  "/category/:category_id/product/:product_id/editProduct",
-  passport.authenticate("jwt", { session: false }),
-  function (req, res) {
-    products.editProduct(
-      req.user.user,
-      req.params.category_id,
-      req.params.product_id,
-      req.body,
-      function (err, dbResult) {
-        if (err) {
-          res.status(500).json(err);
-        } else {
-          if (dbResult.affectedRows == 0) {
-            res
-              .status(400)
-              .json({
-                Status:
-                  400 +
-                  ", Something wrong with product modifying. Try again or contact the IT-manager",
-              });
-          } else {
-            res.status(200).json({ Status: 200 + ", Product changed" });
-          }
+router.post('/category/:category_id/product/:product_id/editProduct', passport.authenticate('jwt', { session: false }),
+function(req, res) {
+  products.editProduct(req.user.idusers, req.params.category_id, req.params.product_id, req.body,
+    function(err, dbResult) {
+      if(err) {
+        res.status(500).json(err);
+      }
+      else {
+        if(dbResult.affectedRows == 0) {
+          res.status(400).json({"Status": 400 + ", Something wrong with product modifying. Try again or contact the IT-manager"});
+        }
+        else {
+          res.status(200).json({"Status": 200 + ", Product changed"});
         }
       }
-    );
-  }
-);
+    });
+});
+
+
 
 // New version of product editing
-router.post(
-  "/:product_id/editProduct",
-  passport.authenticate("jwt", { session: false }),
-  function (req, res) {
-    products.editProductNoCategory(
-      req.user.user,
-      req.params.product_id,
-      req.body,
-      function (err, dbResult) {
-        if (err) {
-          res.status(500).json(err);
-        } else {
-          if (dbResult.affectedRows == 0) {
-            res
-              .status(400)
-              .json({
-                Status:
-                  400 +
-                  ", Something wrong with product modifying. Try again or contact the IT-manager",
-              });
-          } else {
-            res.status(200).json({ Status: 200 + ", Product changed" });
-          }
+router.post('/:product_id/editProduct', passport.authenticate('jwt', { session: false }),
+function(req, res) {
+  products.editProductNoCategory(req.user.idusers, req.params.product_id, req.body,
+    function(err, dbResult) {
+      if(err) {
+        res.status(500).json(err);
+      }
+      else {
+        if(dbResult.affectedRows == 0) {
+          res.status(400).json({"Status": 400 + ", Something wrong with product modifying. Try again or contact the IT-manager"});
+        }
+        else {
+          res.status(200).json({"Status": 200 + ", Product changed"});
         }
       }
-    );
-  }
-);
+    });
+});
+
+
 
 // Modifies product information by productId (with image). Added by Thuc
 router.post(
-  "/:product_id/editProductMultipart",
-  passport.authenticate("jwt", { session: false }),
-  upload.single("product_image"),
-  function (req, res) {
-    const productInfo = { ...req.body };
+  '/:product_id/editProductMultipart', 
+  passport.authenticate('jwt', { session: false }),
+  upload.single('product_image'),
+  function(req, res) {
+    const productInfo = {...req.body};
     productInfo.product_image = req.file.filename;
 
-    products.editProductNoCategoryMultipart(
-      req.user.idusers,
-      req.params.product_id,
-      productInfo,
-      function (err, dbResult) {
-        if (err) {
+    products.editProductNoCategoryMultipart(req.user.idusers, req.params.product_id, productInfo,
+      function(err, dbResult) {
+        if(err) {
           res.status(500).json(err);
-        } else {
-          if (dbResult.affectedRows == 0) {
-            res
-              .status(400)
-              .json({
-                Status:
-                  400 +
-                  ", Something wrong with product modifying. Try again or contact the IT-manager",
-              });
-          } else {
-            res.status(200).json({ Status: 200 + ", Product changed" });
+        }
+        else {
+          if(dbResult.affectedRows == 0) {
+            res.status(400).json({"Status": 400 + ", Something wrong with product modifying. Try again or contact the IT-manager"});
+          }
+          else {
+            res.status(200).json({"Status": 200 + ", Product changed"});
           }
         }
-      }
-    );
+      });
   }
 );
 
+
+
 // Removes the product from the selected category with productId. If not your own created product, product deletion is not possible.
-router.delete(
-  "/category/:category_id/product/:product_id/deleteProduct",
-  passport.authenticate("jwt", { session: false }),
-  function (req, res) {
-    products.deleteProduct(
-      req.user.user,
-      req.params.category_id,
-      req.params.product_id,
-      function (err, dbResult) {
-        if (err) {
-          res.status(500).json(err);
-        } else {
-          if (dbResult.affectedRows == 0) {
-            res
-              .status(400)
-              .json({
-                Status:
-                  400 +
-                  ", Something wrong with removing product from category. Try again or contact the IT-manager",
-              });
-          } else {
-            res.status(200).json({ Status: 200 + ", Product removed" });
-          }
+router.delete('/category/:category_id/product/:product_id/deleteProduct', passport.authenticate('jwt', { session: false }),
+function(req, res) {
+  products.deleteProduct(req.user.idusers, req.params.category_id, req.params.product_id,
+    function(err, dbResult) {
+      if(err) {
+        res.status(500).json(err);
+      }
+      else {
+        if(dbResult.affectedRows == 0) {
+          res.status(400).json({"Status": 400 + ", Something wrong with removing product from category. Try again or contact the IT-manager"})
+        }
+        else {
+          res.status(200).json({"Status": 200 + ', Product removed'});
         }
       }
-    );
-  }
-);
+    });
+});
 
 module.exports = router;
