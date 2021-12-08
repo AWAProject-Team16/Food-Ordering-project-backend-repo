d7774be02bb395fe7c23e414c93064a6c761a8b9
underwaterreal <84696834+underwaterreal@users.noreply.github.com>
2021-12-08 11:53:31 +0200

    Set repo to read-only to prevent accidentally push

--- routes/users.js
+++ routes/users.js
@@ -1,67 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const users = require('../models/users');
+const users = require("../models/users");
 
-const passport = require('passport');
-const Strategy = require('../lib/passportAuth.js');
-const db = require('../lib/database.js');
+const passport = require("passport");
+const Strategy = require("../lib/passportAuth.js");
+const db = require("../lib/database.js");
 
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const { request } = require('express');
-const { session } = require('passport');
-const jwt = require('jsonwebtoken');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { request } = require("express");
+const { session } = require("passport");
+const jwt = require("jsonwebtoken");
 
 router.use(bodyParser.json());
-router.use(cors())
+router.use(cors());
 router.use(passport.initialize());
 
-
-require('dotenv').config();
+require("dotenv").config();
 let jwtSecretKeyLogin = process.env.secret;
-router.post('/login', passport.authenticate('basic', {session: false}), (req, res) => {
+router.post(
+  "/login",
+  passport.authenticate("basic", { session: false }),
+  (req, res) => {
+    const payload = {
+      idusers: req.user.user,
+      account_type: req.user.account_type,
+    };
 
+    const options = {};
 
-  const payload = {
-    idusers : req.user.user,
-    account_type : req.user.account_type
-  };
+    const token = jwt.sign(payload, jwtSecretKeyLogin, options);
+    res.json({ token });
+  }
+);
 
-  const options = {
-  };
-  
-  const token = jwt.sign(payload, jwtSecretKeyLogin, options);
-  res.json({ token });
-
-})
-
-
-router.post('/register', function(req, res) {
-
-
-  users.newUserRegister(req.body,
-    function(err, dbResult) {
-      if(err) {
-        res.status(400).json('err');
-      }
-      else {
-        res.status(201).json({Status: 'Registration success'});
-      }
+router.post("/register", function (req, res) {
+  users.newUserRegister(req.body, function (err, dbResult) {
+    if (err) {
+      res.status(400).json("err");
+    } else {
+      res.status(201).json({ Status: "Registration success" });
+    }
   });
-
 });
 
-
 // Get user information by id. Added by Thuc
-router.get('/myinfo', passport.authenticate('jwt', {session: false}), (req, res) => {
-  users.getUserById(req.user.idusers, (err,dbResult)=>{
-    if (err) {
-      console.error(err);
-      res.sendStatus(500);
-    }
-    else res.json(dbResult);
-  })
-})
-
+router.get(
+  "/myinfo",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    users.getUserById(req.user.idusers, (err, dbResult) => {
+      if (err) {
+        console.error(err);
+        res.sendStatus(500);
+      } else res.json(dbResult);
+    });
+  }
+);
 
 module.exports = router;
