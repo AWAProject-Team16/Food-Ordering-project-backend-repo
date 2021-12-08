bc126682b4c36168f7917db156289b330f924df8
underwaterreal <84696834+underwaterreal@users.noreply.github.com>
2021-12-08 12:04:55 +0200

    Fix wrong credentials

--- models/categories.js
+++ models/categories.js
@@ -1,42 +1,66 @@
-const db = require('../lib/database.js');
+const db = require("../lib/database.js");
 
 const categories = {
+  // getAllCategories: function(callback) {
+  //     return db.query('select * from categories');
+  // },
 
-    // getAllCategories: function(callback) {
-    //     return db.query('select * from categories');
-    // },
+  getRestaurantCategories: function (id, callback) {
+    return db.query(
+      "select * from categories where restaurants_idrestaurants = ?",
+      [id],
+      callback
+    );
+  },
 
-    getRestaurantCategories: function(id, callback) {
-        return db.query('select * from categories where restaurants_idrestaurants = ?', [id], callback);
-    },
+  checkPermissions: function (userId, restaurantId, callback) {
+    return db.query(
+      "select account_type, restaurants.idrestaurants from users JOIN restaurants ON users.idusers = restaurants.users_idusers where idusers = ? AND idrestaurants = ?",
+      [userId, restaurantId],
+      callback
+    );
+  },
 
-    checkPermissions: function(userId, restaurantId, callback) {
-        return db.query('select account_type, restaurants.idrestaurants from users JOIN restaurants ON users.idusers = restaurants.users_idusers where idusers = ? AND idrestaurants = ?', [userId, restaurantId], callback);
-    },
+  addCategory: function (restaurantId, info, callback) {
+    return db.query(
+      "insert into categories (restaurants_idrestaurants, category_name) values (?, ?)",
+      [restaurantId, info.name],
+      callback
+    );
+  },
 
-    addCategory: function(restaurantId, info, callback) {
-        return db.query('insert into categories (restaurants_idrestaurants, category_name) values (?, ?)', [restaurantId, info.name], callback);
-    },
+  renameCategory: function (userId, restaurantId, categoryId, info, callback) {
+    return db.query(
+      "UPDATE categories JOIN restaurants ON categories.restaurants_idrestaurants = restaurants.idrestaurants set category_name=?  \
+        where idcategories = ? AND restaurants_idrestaurants=? AND users_idusers=?",
+      [info.name, categoryId, restaurantId, userId],
+      callback
+    );
+  },
 
-    renameCategory: function(userId, restaurantId, categoryId, info, callback) {
-        return db.query('UPDATE categories JOIN restaurants ON categories.restaurants_idrestaurants = restaurants.idrestaurants set category_name=?  \
-        where idcategories = ? AND restaurants_idrestaurants=? AND users_idusers=?', [info.name, categoryId, restaurantId, userId], callback);
-    },
+  deleteCategory: function (userId, restaurantId, categoryId, callback) {
+    return db.query(
+      "delete categories from categories JOIN restaurants ON categories.restaurants_idrestaurants = restaurants.idrestaurants \
+        where idcategories = ? AND restaurants_idrestaurants=? AND users_idusers=?",
+      [categoryId, restaurantId, userId],
+      callback
+    );
+  },
 
-    deleteCategory: function(userId, restaurantId, categoryId, callback) {
-        return db.query('delete categories from categories JOIN restaurants ON categories.restaurants_idrestaurants = restaurants.idrestaurants \
-        where idcategories = ? AND restaurants_idrestaurants=? AND users_idusers=?', [categoryId, restaurantId, userId], callback);
-    },
-
-    // Added by Thuc
-    getCategoryWithRestaurantNameByCategoryId: function (idusers, idcategories, callback) {
-        return db.query('SELECT categories.*, restaurants.name as restaurant_name FROM categories INNER JOIN restaurants \
+  // Added by Thuc
+  getCategoryWithRestaurantNameByCategoryId: function (
+    idusers,
+    idcategories,
+    callback
+  ) {
+    return db.query(
+      "SELECT categories.*, restaurants.name as restaurant_name FROM categories INNER JOIN restaurants \
         ON categories.restaurants_idrestaurants = restaurants.idrestaurants \
-        WHERE users_idusers = ? AND idcategories = ?',
-        [idusers, idcategories],
-        callback);
-    },
-
-}
+        WHERE users_idusers = ? AND idcategories = ?",
+      [idusers, idcategories],
+      callback
+    );
+  },
+};
 
 module.exports = categories;
